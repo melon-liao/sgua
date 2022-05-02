d20f8cd79083f6c296aaa7859863243dd54ed45e
melon <melon@mail.com>
2022-05-02 17:22:56 +0800

    init

---
+++ sgua/Hubs/ChatHub.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace sgua.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public static Task<RedisConnection> _redisConnectionFactory;
+        private RedisConnection _redisConnection;
+
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            _redisConnection = await ChatHub._redisConnectionFactory;
+            await _redisConnection.BasicRetryAsync(async (db) => await db.SetAddAsync("online", Context.ConnectionId));
+            await UpdateOnline();
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            _redisConnection = await ChatHub._redisConnectionFactory;
+            await _redisConnection.BasicRetryAsync(async (db) => await db.SetRemoveAsync("online", Context.ConnectionId));
+            await UpdateOnline();
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        private async Task UpdateOnline()
+        {
+            var online = await _redisConnection.BasicRetryAsync(async (db) => await db.SetMembersAsync("online"));
+            await Clients.All.SendAsync(
+                "UpdateOnline",
+                online?.Length,
+                DateTime.Now.Ticks
+            );
+        }
+
+        //public ChatHub()
+        //{
+        //    _redisConnectionFactory = RedisConnection.InitializeAsync();
+        //}
+    }
+}
---
+++ sgua/Program.cs
@@ -0,0 +1,32 @@
+using sgua;
+using sgua.Hubs;
+using Azure.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+ChatHub._redisConnectionFactory = RedisConnection.InitializeAsync(builder.Configuration["CacheConnection"].ToString());
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapHub<ChatHub>("/chatHub");
+
+app.Run();
